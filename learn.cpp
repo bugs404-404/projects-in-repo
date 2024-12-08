633f640791f1b346be5d81052231bf5791c1e389
bugs <hancycoder004@gmail.com>
2024-12-08 03:12:08 +0300

    cpp 101

---
+++ learn.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(){
+    // this is first comment from cpp
+    std::cout << "i like games "<<"\n";
+    std::cout << "i love music too"<<"\n";
+     int x = 45;
+     int y = 45;
+     int sum = x + y;
+     std::cout << sum <<"\n";
+     std::cout <<"that is my age";
+    return 0;
+}
